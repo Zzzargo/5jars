9f6843152c145056cf50472629d4dfb9c169e5b0
Vasea <zzzargoman@gmail.com>
2026-09-10 00:49:47 +0300

    Improved the cobol engine by reviewing the native functions invocations Sprinkled some tests on top, results are good but I still get some segfault in the jvm
    Signed-off-by: Vasea <zzzargoman@gmail.com>

--- backend/spring_server/src/main/java/com/zargo/fivejars/spring_server/cobol_bridge/CobolEngine.java
+++ backend/spring_server/src/main/java/com/zargo/fivejars/spring_server/cobol_bridge/CobolEngine.java
@@ -1,5 +1,6 @@
 package com.zargo.fivejars.spring_server.cobol_bridge;
 
+import com.zargo.fivejars.spring_server.common.exceptions.CobolExecutionException;
 import org.springframework.stereotype.Component;
 
 import java.lang.foreign.*;
@@ -9,23 +10,35 @@ import java.nio.file.Path;
 
 @Component
 public class CobolEngine {
+    // All COBOL PICs used for money are S9(13)V99 =>
+    //      15 digits + 1 sign = 16 / 2 = 8 bytes for a number in COMP-3 format
+    private static final int COMP3_NUMS_DIGITS = 15;
+    private static final int COMP3_NUMS_DECIMALS = 2;
+    private static final int COMP3_NUMS_BYTELEN = (COMP3_NUMS_DIGITS + 1) / 2;
+
     private final MethodHandle depositHandle;
     private final MethodHandle testStringHandle;
 
     public CobolEngine() {
-        // Load the shared library
-        System.load(Path.of("libfivejars_cobol_kernel.so").toAbsolutePath().toString());
-        SymbolLookup lookup = SymbolLookup.loaderLookup();
-        Linker linker = Linker.nativeLinker();
+        final SymbolLookup lookup = SymbolLookup.loaderLookup();
+        final Linker linker = Linker.nativeLinker();
 
-        FunctionDescriptor initDescriptor = FunctionDescriptor.ofVoid();
-        MethodHandle cobInitHandle = linker.downcallHandle(lookup.find("cob_init").orElseThrow(), initDescriptor);
-
+        // Load the cobol runtime library and initialize the runtime
+        System.loadLibrary("cob");
+        // TODO: get rid of this error <attempt to reference invalid memory address (signal)> by getting cob_init type
+        //  void cob_init(int argc, char **argv) ?
+        final MemorySegment initMemorySegment = lookup.find("cob_init").orElseThrow(RuntimeException::new);
+        final FunctionDescriptor initDescriptor = FunctionDescriptor.ofVoid(ValueLayout.JAVA_INT, ValueLayout.ADDRESS);
+        final MethodHandle cobInitHandle = linker.downcallHandle(initMemorySegment, initDescriptor);
         try {
-            cobInitHandle.invokeExact();
-        } catch (Throwable ex) {
-            System.out.println("Da ya sho ebanulsya");
+            cobInitHandle.invokeExact(0, MemorySegment.NULL);
+        } catch (Throwable e) {
+            System.out.println("Error calling \"cob_init\": " + e.getMessage());
         }
+
+        // Load the shared library
+        // TODO: get the library from the COBOL root directory
+        System.load(Path.of("libfivejars_cobol_kernel.so").toAbsolutePath().toString());
 
         this.depositHandle = linker.downcallHandle(
                 lookup.find("DEPOSIT").orElseThrow(),
@@ -38,37 +51,37 @@ public class CobolEngine {
         );
     }
 
-    // Generic execution wrapper
-    public void call(String programId, Object... args) {
-    }
-
     public BigDecimal deposit(final BigDecimal balance, final BigDecimal amount) {
         try (Arena arena = Arena.ofConfined()) {
             MemorySegment balanceSeg = arena.allocateFrom(
-                    ValueLayout.JAVA_BYTE, CobolConverter.decimalToComp3(balance, 15, 3)
+                    ValueLayout.JAVA_BYTE, CobolConverter.decimalToComp3(
+                            balance, COMP3_NUMS_DIGITS, COMP3_NUMS_DECIMALS
+                    )
             );
             MemorySegment amountSeg = arena.allocateFrom(
-                    ValueLayout.JAVA_BYTE, CobolConverter.decimalToComp3(amount, 15, 3)
+                    ValueLayout.JAVA_BYTE, CobolConverter.decimalToComp3(
+                            amount, COMP3_NUMS_DIGITS, COMP3_NUMS_DECIMALS
+                    )
             );
-            MemorySegment resultSeg = arena.allocate(8);
+            MemorySegment resultSeg = arena.allocate(COMP3_NUMS_BYTELEN);
 
             depositHandle.invokeExact(balanceSeg, amountSeg, resultSeg);
 
-            return CobolConverter.comp3ToDecimal(resultSeg.toArray(ValueLayout.JAVA_BYTE), 3);
+            return CobolConverter.comp3ToDecimal(resultSeg.toArray(ValueLayout.JAVA_BYTE), COMP3_NUMS_DECIMALS);
         } catch (Throwable e) {
-            throw new RuntimeException("COBOL Execution Failed", e);
+            throw new CobolExecutionException(e.getMessage());
         }
     }
 
     public String checkString() {
         try (Arena arena = Arena.ofConfined()) {
             // A non null-terminated string of max 20 alphanumerics
-            MemorySegment resultSeg = arena.allocate(20);
-            testStringHandle.invoke(resultSeg);
+            MemorySegment resultSeg = arena.allocateFrom(ValueLayout.JAVA_BYTE, new byte[20]);
+            testStringHandle.invokeExact(resultSeg);
             var cStyleString = resultSeg.toArray(ValueLayout.JAVA_BYTE);
             return new String(cStyleString).trim();
         } catch (Throwable e) {
-            throw new RuntimeException("COBOL Execution Failed", e);
+            throw new CobolExecutionException(e.getMessage());
         }
     }
 }
--- backend/spring_server/src/test/java/com/zargo/fivejars/spring_server/cobol_bridge/CobolEngineTest.java
+++ backend/spring_server/src/test/java/com/zargo/fivejars/spring_server/cobol_bridge/CobolEngineTest.java
@@ -28,10 +28,53 @@ public class CobolEngineTest {
     }
 
     @Test
+    @DisplayName("1.01 + 2.2 should be equal to 3.21")
+    void shouldHandleDecimalsCorrectly() {
+        BigDecimal currBalance = new BigDecimal("1.01");
+        BigDecimal amountToDeposit = new BigDecimal("2.2");
+
+        BigDecimal res = engine.deposit(currBalance, amountToDeposit);
+
+        assertEquals(0, new BigDecimal("3.21").compareTo(res));
+    }
+
+    @Test
+    @DisplayName("2.014 + 2.986 should be equal to 5.00")
+    void shouldRoundFinerDecimalsCorrectly() {
+        BigDecimal currBalance = new BigDecimal("2.014");
+        BigDecimal amountToDeposit = new BigDecimal("2.986");
+
+        BigDecimal res = engine.deposit(currBalance, amountToDeposit);
+
+        assertEquals(0, new BigDecimal("5.00").compareTo(res));
+    }
+
+    @Test
+    @DisplayName("-5.21 + 2.21 should be equal to -3.00")
+    void shouldAddNegativeNumbersCorrectly() {
+        BigDecimal currBalance = new BigDecimal("-5.21");
+        BigDecimal amountToDeposit = new BigDecimal("2.21");
+
+        BigDecimal res = engine.deposit(currBalance, amountToDeposit);
+
+        assertEquals(0, new BigDecimal("-3.00").compareTo(res));
+    }
+
+    @Test
+    @DisplayName("-1520382765446.207 + 2298204982546.777 should be equal to 777822217100.570")
+    void testLargeSignRoundingDecimalExcess() {
+        BigDecimal currBalance = new BigDecimal("-1520382765446.207");
+        BigDecimal amountToDeposit = new BigDecimal("2298204982546.777");
+
+        BigDecimal res = engine.deposit(currBalance, amountToDeposit);
+
+        assertEquals(0, new BigDecimal("777822217100.570").compareTo(res));
+    }
+
+    @Test
     @DisplayName("Should get the string \"Hi from COBOL!\" from COBOL")
     void shouldGetAStringFromCobol() {
         var string = engine.checkString();
-        assertNotNull(string, "string should not be null");
-        System.out.println(string);
+        assertEquals("Hi from COBOL!", string);
     }
 }
